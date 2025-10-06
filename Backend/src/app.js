f971d2b80a426ae64448114dfb5a8d769fa06d13
Pratyush Kumar Sinha <pratyushsinha2003@gmail.com>
2025-10-07 00:22:44 +0530

    updated cors urls

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,14 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",               // local frontend
+    "https://https://virtual-hangout-frontend.onrender.com/" // deployed frontend
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.json({limit : "40kb"}));
 app.use(express.urlencoded({limit : "40kb" , extended : true}))
