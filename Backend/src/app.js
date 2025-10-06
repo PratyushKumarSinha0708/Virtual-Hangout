18b7a95b411f5c2bea5b9faffd0d00d8cc1b601c
Pratyush Kumar Sinha <pratyushsinha2003@gmail.com>
2025-10-07 00:57:46 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ const io = connectToSocket(server);
 app.use(cors({
   origin: [
     "http://localhost:5173",               // local frontend
-    "hhttps://virtual-hangout-backend.onrender.com/api/v1/users/login" // deployed frontend
+    "hhttps://virtual-hangout-backend.onrender.com" // deployed frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
